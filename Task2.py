c8ce0c7e3eb5731ecca9495799460e0fb911cb1f
Andrew Zubkov <99863211+METALROK@users.noreply.github.com>
2025-06-12 03:34:29 +0300

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -2,9 +2,9 @@ import random
 
 
 def generate_geometric_progression():
-    length = random.randint(7, 10)  
-    start = random.randint(1, 10) 
-    ratio = random.randint(2, 5) 
+    length = random.randint(7, 10)
+    start = random.randint(1, 10)
+    ratio = random.randint(2, 5)
 
     progression = []
     for i in range(length):
@@ -12,35 +12,36 @@ def generate_geometric_progression():
 
     return progression
 
+def progression_game():
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f"Hello, {name}!")
+    print("What number is missing in the progression?")
 
-print("Welcome to the Brain Games!")
-name = input("May I have your name? ")
-print(f"Hello, {name}!")
-print("What number is missing in the progression?")
+    for _ in range(3):
+        progression = generate_geometric_progression()
+        hidden_index = random.randint(0, len(progression) - 1)
+        correct_answer = progression[hidden_index]
 
-while True:
-    progression = generate_geometric_progression()
-    hidden_index = random.randint(0, len(progression) - 1)
-    correct_answer = progression[hidden_index]
+        progression_str = []
+        for i, num in enumerate(progression):
+            if i == hidden_index:
+                progression_str.append("..")
+            else:
+                progression_str.append(str(num))
+        question = " ".join(progression_str)
 
-    progression_str = []
-    for i, num in enumerate(progression):
-        if i == hidden_index:
-            progression_str.append("..")
+        print(f"Question: {question}")
+        user_answer = input("Your answer: ").strip()
+
+        if int(user_answer) == correct_answer:
+            print("Correct!")
         else:
-            progression_str.append(str(num))
-    question = " ".join(progression_str)
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
 
-    print(f"Question: {question}")
-    user_answer = input("Your answer: ").strip()
+    print(f"Congratulations, {name}!")
 
-    if user_answer == 'q':
-        break
 
-    if int(user_answer) == correct_answer:
-        print("Correct!")
-    else:
-        print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-        print(f"Let's try again, {name}!")
-
-print(f"Congratulations, {name}!")
+progression_game()
